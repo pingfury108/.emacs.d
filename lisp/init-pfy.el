237be9f58d94f0dbf4a0973b58a93b7653acd904
pingfury <pingfury@outlook.com>
2024-09-03 12:09:32 +0800

    add gptel

--- lisp/init-pfy.el
+++ lisp/init-pfy.el
@@ -1,12 +1,15 @@
 ;;; pfy.el --- pfy  -*- lexical-binding: t; -*-
 
 ;;; Code:
-(use-package insert-translated-nme
-  :straight '(insert-translated-nme
+(use-package insert-translated-name
+  :straight '(insert-translated-name
               :type git
               :host github
               :repo "manateelazycat/insert-translated-name")
   :commands (insert-translated-name-insert))
+
+(use-package gptel
+  :straight 'gptel)
 
 (provide 'init-pfy)
 ;;; pfy.el ends here
