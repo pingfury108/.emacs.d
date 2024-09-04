c52145513cd8280b0ed7976017bd7b2aa59d64a8
pingfury <pingfury@outlook.com>
2024-09-04 10:27:18 +0800

    fix gptel

--- lisp/init-pfy.el
+++ lisp/init-pfy.el
@@ -9,7 +9,13 @@
   :commands (insert-translated-name-insert))
 
 (use-package gptel
-  :straight 'gptel)
+  :straight 'gptel
+  :init
+  (setq
+   gptel-model "gemini-pro"
+   gptel-backend (gptel-make-gemini "Gemini"
+                   :key gptel-gemini-api-key
+                   :stream t)))
 
 (provide 'init-pfy)
 ;;; pfy.el ends here
